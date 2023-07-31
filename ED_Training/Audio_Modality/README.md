c41f5cc8a4fce7583c19917cf0c834338b2a8e8b
Annanda Sousa <annanda.sousa@gmail.com>
2023-07-31 20:06:43 +0100

    Creating the structure of the ED training for audio modality (E4).

---
+++ ED_Training/Audio_Modality/README.md
@@ -0,0 +1,4 @@
+# Emotion Detection Training - Audio Modality
+
+Project to train an ED model using audio data input type. 
+
---
+++ ED_Training/Audio_Modality/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='audio_modality',
+    version='0.1',
+    description='Training an ED model using audio data input type.',
+    author='UM-XR2Learn-Enablers',
+    packages=['audio_modality'],
+    zip_safe=False
+)
