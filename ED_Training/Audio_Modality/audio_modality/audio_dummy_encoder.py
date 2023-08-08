42679df491672adceeecb96b153ef1de681511cf
Annanda Sousa <annanda.sousa@gmail.com>
2023-08-08 16:49:06 +0100

    Saving RAVDESS dataset into a CSV file with labels and path to audio files. Starting to create function to generate a dummy dataset of features and labels.

--- ED_Training/Audio_Modality/audio_modality/audio_dummy_encoder.py
+++ ED_Training/Audio_Modality/audio_modality/audio_dummy_encoder.py
@@ -4,35 +4,53 @@ import pandas as pd
 import os
 import glob
 
-from conf import DATA_PATH, EMOTIONS
+from conf import DATA_PATH, EMOTIONS, EMOTION_INTENSITY, RAVDESS_DATA_PATH
 
-data_frame = pd.DataFrame(columns=['Emotion', 'Emotion intensity', 'Gender', 'Path', 'Timestamp'])
 
-path_files = glob.glob(os.path.join(DATA_PATH, '*', '*.wav'))
+def create_and_save_dataset():
+    """" Function to create and save a dataset in CSV form from the RAVDESS dataset.
+    """
+    columns_df = ['Emotion', 'Emotion Intensity', 'Gender', 'Path', 'Timestamp']
+    df = pd.DataFrame(columns=columns_df)
 
-for path_file in path_files:
-    file_name = os.path.basename(path_file)
+    path_files = glob.glob(os.path.join(DATA_PATH, '*', '*.wav'))
 
-for dirname, _, filenames in os.walk(DATA_PATH):
-    for filename in filenames:
-        file_path = os.path.join('/kaggle/input/', dirname, filename)
-        identifiers = filename.split('.')[0].split('-')
-        emotion = (int(identifiers[2]))
-        if emotion == 8:
-            emotion = 0
-        if int(identifiers[3]) == 1:
-            emotion_intensity = 'normal'
-        else:
-            emotion_intensity = 'strong'
+    for path_file in path_files:
+        file_name = os.path.basename(path_file)
+        file_name = file_name.replace('.wav', '')
+        identifiers = file_name.split('-')
         if int(identifiers[6]) % 2 == 0:
             gender = 'female'
         else:
             gender = 'male'
+        new_row_dict = {'Emotion': EMOTIONS[int(identifiers[2])],
+                        'Emotion Intensity': EMOTION_INTENSITY[int(identifiers[3])],
+                        'Gender': gender,
+                        'Path': path_file
+                        }
+        # new_row = pd.DataFrame.from_dict(new_row_dict, ignore_index=True)
+        df = pd.concat([df, pd.DataFrame([new_row_dict])], ignore_index=True)
+    df.to_csv(os.path.join(RAVDESS_DATA_PATH, 'ravdess_dataset_with_paths.csv'), index=False)
 
-        data_frame = data_frame.append({"Emotion": emotion,
-                                        "Emotion intensity": emotion_intensity,
-                                        "Gender": gender,
-                                        "Path": file_path
-                                        },
-                                       ignore_index=True
-                                       )
+
+def generate_dataset_features(path_dataset):
+    """
+    Generates dataset from features extracted from wav2vec2 and save it into a CSV file.
+    :param path_dataset: string. The path for CSV file with the dataset containing labels and path to audio files.
+    :return: None
+    """
+    path_df = pd.read_csv(path_dataset)
+    df_features = pd.DataFrame(columns=['Emotion', 'Features'])
+    for row in path_df:
+        new_row_features = {}
+        # Call wav2vec2 to extract features
+        df_features = pd.concat([df_features, pd.DataFrame([new_row_features])], ignore_index=True)
+        pass
+
+    df_features.to_csv(os.path.join(RAVDESS_DATA_PATH, 'ravdess_dataset_features.csv'), index=False)
+
+
+if __name__ == '__main__':
+    # create_and_save_dataset()
+    path_dataset = os.path.join(RAVDESS_DATA_PATH, 'ravdess_dataset_with_paths.csv')
+    generate_dataset_features(path_dataset)
--- ED_Training/Audio_Modality/audio_modality/conf.py
+++ ED_Training/Audio_Modality/audio_modality/conf.py
@@ -5,7 +5,10 @@ from decouple import config
 EMOTIONS = {1: 'neutral', 2: 'calm', 3: 'happy', 4: 'sad',
             5: 'angry', 6: 'fear', 7: 'disgust',
             8: 'surprise'}
+EMOTION_INTENSITY = {1: 'normal', 2: "strong"}
+
 SAMPLE_RATE = 48000
 
 main_folder = pathlib.Path(__file__).parent.parent.absolute()
 DATA_PATH = os.path.join(main_folder, 'datasets', 'RAVDESS', 'audio_speech_actors_01-24')
+RAVDESS_DATA_PATH = os.path.join(main_folder, 'datasets', 'RAVDESS')
