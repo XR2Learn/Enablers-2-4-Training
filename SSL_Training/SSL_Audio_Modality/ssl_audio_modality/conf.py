19006f733fbffe1a708eb9d3ff4e369a224b877d
Annanda Sousa <annanda.sousa@gmail.com>
2023-08-31 17:25:40 +0100

    Starting to create a scheme for configuration management.

--- SSL_Training/SSL_Audio_Modality/ssl_audio_modality/conf.py
+++ SSL_Training/SSL_Audio_Modality/ssl_audio_modality/conf.py
@@ -1,7 +1,25 @@
 """
 File to include global variables across the python package and configuration.
-All the other files inside the python package can access this variables.
+All the other files inside the python package can access these variables.
 """
 from decouple import config
+import os
+import pathlib
+import json
 
-TESTING = 'Importing variable from conf.py'
+TESTING = 'Importing a variable from conf.py'
+
+MAIN_FOLDER_DEFAULT = pathlib.Path(__file__).parent.parent.absolute()
+MAIN_FOLDER = config('MAIN_FOLDER', default=MAIN_FOLDER_DEFAULT)
+
+# Yet to check if this is really necessary, maybe only for cases where passing values as ENV VARS is too cumbersome
+# e.g. [[1, 'a', ],['789', 'o', 9]] would be very annoying to write and parse.
+CUSTOM_SETTINGS = {
+    'key': {
+        'default': 'value',
+    }
+}
+PATH_CUSTOM_SETTINGS = os.path.join(MAIN_FOLDER, 'configuration.json')
+if os.path.exists(PATH_CUSTOM_SETTINGS):
+    with open(PATH_CUSTOM_SETTINGS, 'r') as f:
+        CUSTOM_SETTINGS = json.load(f)
