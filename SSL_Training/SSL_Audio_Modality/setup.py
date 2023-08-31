82eb9ecba75cc9428a50b7d4e7fbf7c839833b0b
Annanda Sousa <annanda.sousa@gmail.com>
2023-08-31 10:22:54 +0100

    Updating requirements, adding setup.py and conf.py in SSL training structure.

---
+++ SSL_Training/SSL_Audio_Modality/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='ssl_audio_modality',
+    version='0.1',
+    description='Pre-training a SSL encoder model using audio data input type.',
+    author='UM-XR2Learn-Enablers',
+    packages=['ssl_audio_modality'],
+    zip_safe=False
+)
---
+++ SSL_Training/SSL_Audio_Modality/ssl_audio_modality/conf.py
@@ -0,0 +1,5 @@
+"""
+File to include global variables across the python package and configuration.
+All the other files inside the python package can access this variables.
+"""
+from decouple import config
