79c3312496f1181337e940aa8fa1231e49653398
Annanda Sousa <annanda.sousa@gmail.com>
2023-09-07 16:03:35 +0100

    Adding some printing code to verify docker running and accessing configuration file.

--- Supervised_Training/Supervised_Audio_Modality/supervised_audio_modality/train.py
+++ Supervised_Training/Supervised_Audio_Modality/supervised_audio_modality/train.py
@@ -1,3 +1,16 @@
 # Python code here
 
-print('Docker for Ed-Training Audio has run.')
+from conf import CUSTOM_SETTINGS
+
+
+def example_run():
+    """
+    A basic print function to verify if docker is running.
+    :return: None
+    """
+    model_config = CUSTOM_SETTINGS['model']['some_config_model']
+    print(f'Docker for Ed-Training Audio has run. Conf from configuration.json file: {model_config}')
+
+
+if __name__ == '__main__':
+    example_run()
