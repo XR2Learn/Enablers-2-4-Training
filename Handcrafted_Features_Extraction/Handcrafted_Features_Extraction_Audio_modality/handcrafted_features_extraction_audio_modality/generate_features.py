fa227045830111a4e9213d39e174b643a511a1e2
PedroDKE <pedroantonio.jeuris@gmail.com>
2023-09-20 14:21:13 +0200

    support for egemaps

--- Handcrafted_Features_Extraction/Handcrafted_Features_Extraction_Audio_modality/handcrafted_features_extraction_audio_modality/generate_features.py
+++ Handcrafted_Features_Extraction/Handcrafted_Features_Extraction_Audio_modality/handcrafted_features_extraction_audio_modality/generate_features.py
@@ -2,7 +2,9 @@
 import os
 import numpy as np
 import pathlib
+import scipy
 import torch
+import opensmile
 import torchaudio
 import torchaudio.transforms as transforms
 
@@ -28,14 +30,16 @@ def extract_handcrafted_features():
     print(f"the features that will be extracted, if supported, and saved will be: {features_to_extract}")
 
     full_data_paths = [os.path.join(processed_data_folder,data_path) for data_path in all_data_paths]
+
     if "MFCC" in features_to_extract:
         save_folder = os.path.join(OUTPUTS_FOLDER,'handcrafted-features-generation-audio','MFCC')
         pathlib.Path(save_folder).mkdir(parents=True, exist_ok=True)
         extract_and_save_MFCC(full_data_paths,save_folder)
 
     if "eGeMAPs" in features_to_extract:
-        print("eGeMAPs not supported yet")
-        #extract_and_save_MFCC(all_data_paths)
+        save_folder = os.path.join(OUTPUTS_FOLDER,'handcrafted-features-generation-audio','eGeMAPs')
+        pathlib.Path(save_folder).mkdir(parents=True, exist_ok=True)
+        extract_and_save_egemaps(full_data_paths,save_folder)
 
     if "Spectogram" in features_to_extract:
         save_folder = os.path.join(OUTPUTS_FOLDER,'handcrafted-features-generation-audio','Spectograms')
@@ -76,9 +80,22 @@ def extract_and_save_spectogram(all_data_paths,save_folder):
     """
     for data_path in tqdm(all_data_paths,desc='Extracting Spectograms'):
         waveform, sample_rate = torchaudio.load(data_path)
-        transform = transforms.Spectrogram(n_fft=800)
+        transform = transforms.Spectrogram(n_fft=400)
         spectogram = transform(waveform).numpy()
         np.save(os.path.join(save_folder,data_path.split(os.path.sep)[-1][:-4]),spectogram)
+
+def extract_and_save_egemaps(all_data_paths,save_folder):
+    for data_path in tqdm(all_data_paths,desc='Extracting eGeMAPs'):
+        sr,audio = scipy.io.wavfile.read(data_path)
+        smile_egemaps = opensmile.Smile(
+                            feature_set=opensmile.FeatureSet.eGeMAPSv02,
+                            feature_level=opensmile.FeatureLevel.Functionals,
+                        )
+        #TODO: double check if the padding actually gets trimmed
+        #TODO: check if egemaps need standardized N(mean=0,std=1) or normalized[-1,1] audio
+        audio = np.trim_zeros(audio)
+        egemaps = smile_egemaps.process_signal(audio, sr)
+        np.save(os.path.join(save_folder,data_path.split(os.path.sep)[-1][:-4]),egemaps)
 
 
 if __name__ == '__main__':
