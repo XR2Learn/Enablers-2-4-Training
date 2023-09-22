e6092342101370048abb77bb2040fcf21ede57d9
PedroDKE <pedroantonio.jeuris@gmail.com>
2023-09-22 12:58:50 +0200

    first pre-train augmentations still have to be tested and saving of moddel/logging needs some polishing training seems to run without errors

--- SSL_Training/SSL_Audio_Modality/ssl_audio_modality/pre_train.py
+++ SSL_Training/SSL_Audio_Modality/ssl_audio_modality/pre_train.py
@@ -1,16 +1,81 @@
 # Python code here
 
-from conf import CUSTOM_SETTINGS
+from pytorch_lightning import Trainer, seed_everything
+from conf import CUSTOM_SETTINGS,MAIN_FOLDER
+from ssl_dataset import SSLDataModule
+from callbacks.setup_callbacks import setup_callbacks
+from utils.init_utils import (init_augmentations, init_datamodule,
+                              init_loggers, init_random_split, init_transforms,
+                              setup_ssl_model)
+from utils.utils import copy_file, generate_experiment_id, load_yaml_to_dict
 
+from encoders.cnn1d import CNN1D,CNN1D1L
+from ssl_methods.SimCLR import SimCLR
 
-def example_run():
-    """
-    A basic print function to verify if docker is running.
-    :return: None
-    """
-    model_config = CUSTOM_SETTINGS['model']['some_config_model']
-    print(f'Docker for SSL-Pre-Training Audio has run (Enabler 2). Conf from configuration.json file: {model_config}')
+def run_pre_training():
+    print(CUSTOM_SETTINGS)
+    splith_paths = {'train':"outputs/train.csv",'val':"outputs/val.csv",'test':"outputs/test.csv"}
 
+    train_transforms = {}
+    test_transforms = {}
+    #for modality in  preprocessing_configs['sensors'].keys():
+    #    transform_cfg =  preprocessing_configs['sensors'][modality]['transforms']
+    #    cur_train_transforms, cur_test_transforms = init_transforms(modality, transform_cfg)
+    #    train_transforms.update(cur_train_transforms)
+    #    test_transforms.update(cur_test_transforms)
+    if 'transforms' in CUSTOM_SETTINGS.keys():
+        train_transforms,test_transforms = init_transforms(CUSTOM_SETTINGS['transforms'])
+
+    if 'augmentations' in CUSTOM_SETTINGS.keys():
+        augmentations = init_augmentations(CUSTOM_SETTINGS['augmentations'])
+
+    datamodule = SSLDataModule(
+        path=MAIN_FOLDER,
+        batch_size=128,
+        split=splith_paths,
+        train_transforms=train_transforms,
+        test_transforms=test_transforms,
+        n_views=2,
+        num_workers=2,
+        augmentations=augmentations
+    )
+    #initialise encoder
+    encoder = CNN1D(in_channels=1,
+                    len_seq=CUSTOM_SETTINGS["pre_processing_config"]['max_length']*CUSTOM_SETTINGS["pre_processing_config"]['target_sr'],
+                    out_channels=[2,2,2],
+                    kernel_sizes=[7,7,7],
+                    stride=4
+                    )
+    #initialise ssl model with configured SLL method
+    ssl_model = SimCLR(encoder=encoder,ssl_batch_size=128)
+
+    print(ssl_model)
+    #init callbacks  # initialize callbacks
+    callbacks = setup_callbacks(
+        early_stopping_metric="val_loss",
+        no_ckpt=True,
+        patience=5
+    )
+    # initialize Pytorch-Lightning Training
+    trainer = Trainer(
+        #logger=loggers,
+        #accelerator='cpu' if args.gpus == 0 else 'gpu',
+        #devices=None if args.gpus == 0 else args.gpus,
+        #deterministic=True, 
+        #max_epochs=num_epochs, 
+        #default_root_dir=args.outputs_path, 
+        callbacks=callbacks,
+        max_epochs=5
+    )
+
+    # pre-train and report test loss
+    trainer.fit(ssl_model, datamodule)
+    metrics = trainer.test(ssl_model, datamodule, ckpt_path='best')
+    print(metrics)
+
+    #train ssl model
+    #save the encoder weights
+    pass
 
 if __name__ == '__main__':
-    example_run()
+    run_pre_training()
