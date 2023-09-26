1a29ddb7aa1d0bf96da9b3736c2b6eef91c8181d
PedroDKE <pedroantonio.jeuris@gmail.com>
2023-09-26 10:45:13 +0200

    sup training

--- Supervised_Training/Supervised_Audio_Modality/supervised_audio_modality/train.py
+++ Supervised_Training/Supervised_Audio_Modality/supervised_audio_modality/train.py
@@ -12,6 +12,8 @@ from utils.init_utils import (init_augmentations, init_datamodule,
 from utils.utils import copy_file, generate_experiment_id, load_yaml_to_dict
 
 from encoders.cnn1d import CNN1D,CNN1D1L
+from classification_model import classification_model
+from classifiers.linear import LinearClassifier
 
 def run_supervised_training():
     print(CUSTOM_SETTINGS)
@@ -19,26 +21,23 @@ def run_supervised_training():
 
     train_transforms = {}
     test_transforms = {}
-    #for modality in  preprocessing_configs['sensors'].keys():
-    #    transform_cfg =  preprocessing_configs['sensors'][modality]['transforms']
-    #    cur_train_transforms, cur_test_transforms = init_transforms(modality, transform_cfg)
-    #    train_transforms.update(cur_train_transforms)
-    #    test_transforms.update(cur_test_transforms)
+
     if 'transforms' in CUSTOM_SETTINGS.keys():
         train_transforms,test_transforms = init_transforms(CUSTOM_SETTINGS['transforms'])
 
-    if 'augmentations' in CUSTOM_SETTINGS.keys():
-        augmentations = init_augmentations(CUSTOM_SETTINGS['augmentations'])
+    # for now, don't use augmentations during supervised training
+    #if 'augmentations' in CUSTOM_SETTINGS.keys():
+    #    augmentations = init_augmentations(CUSTOM_SETTINGS['augmentations'])
 
     datamodule = SupervisedDataModule(
         path=MAIN_FOLDER,
-        batch_size=CUSTOM_SETTINGS['ssl_config']['batch_size'],
+        batch_size=CUSTOM_SETTINGS['sup_config']['batch_size'],
         split=splith_paths,
         train_transforms=train_transforms,
         test_transforms=test_transforms,
         n_views=2,
         num_workers=2,
-        augmentations=augmentations
+        #augmentations=augmentations
     )
     #initialise encoder
     encoder = CNN1D(in_channels=1,
@@ -48,7 +47,8 @@ def run_supervised_training():
                     stride=4
                     )
     #add classification head to encoder
-    #ssl_model = SimCLR(encoder=encoder,ssl_batch_size=CUSTOM_SETTINGS['ssl_config']['batch_size'],**CUSTOM_SETTINGS['ssl_config']['kwargs'])
+    classifier = LinearClassifier(encoder.out_size,8) 
+    model = classification_model(encoder=encoder,classifier=classifier)
 
     #print(ssl_model)
     #init callbacks  # initialize callbacks
@@ -63,18 +63,18 @@ def run_supervised_training():
         #accelerator='cpu' if args.gpus == 0 else 'gpu',
         #devices=None if args.gpus == 0 else args.gpus,
         deterministic=True, 
-        default_root_dir=os.path.join(MAIN_FOLDER,'outputs','SSL_Training'),
+        default_root_dir=os.path.join(MAIN_FOLDER,'outputs','Sup_Training'),
         callbacks=callbacks,
-        max_epochs=CUSTOM_SETTINGS['ssl_config']['epochs']
+        max_epochs=CUSTOM_SETTINGS['sup_config']['epochs']
     )
 
     # pre-train and report test loss
-    #trainer.fit(ssl_model, datamodule)
-    #metrics = trainer.test(ssl_model, datamodule, ckpt_path='best')
-    #print(metrics)
+    trainer.fit(model, datamodule)
+    metrics = trainer.test(model, datamodule, ckpt_path='best')
+    print(metrics)
 
     #save weights
-    torch.save(encoder.state_dict(), os.path.join(MAIN_FOLDER,'outputs','SSL_Training','test_encoder.pt'))
+    #torch.save(encoder.state_dict(), os.path.join(MAIN_FOLDER,'outputs','Sup_Training','test_model.pt'))
 
     pass
 
