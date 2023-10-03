c741f72a636997d0ed1ccd6280c76a0a5bcb097b
Annanda Sousa <annanda.sousa@gmail.com>
2023-10-03 12:52:42 +0200

    Creating an example of unittest for SL_training_audio.

--- Supervised_Training/Supervised_Audio_Modality/tests/test_example.py
+++ Supervised_Training/Supervised_Audio_Modality/tests/test_example.py
@@ -4,12 +4,13 @@ from unittest.mock import patch
 
 class MockedServiceStreamTestCase(unittest.TestCase):
     def setUp(self):
-        self.something = Something()  # run before every test
+        # self.something = Something()
+        pass# run before every test
 
     def tearDown(self):  # run after every test
         pass
 
-    @patch('my_project.MyClass.my_method_to_be_mocked')
-    def test_something_calls_method(self, mocked_method):
-        self.something.run()
-        self.assertTrue(mocked_method.called)
+    # @patch('my_project.MyClass.my_method_to_be_mocked')
+    # def test_something_calls_method(self, mocked_method):
+    #     self.something.run()
+    #     self.assertTrue(mocked_method.called)
