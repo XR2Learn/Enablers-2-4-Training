892bb2e063aac92f4109db694a60c7072856b4b9
PedroDKE <pedroantonio.jeuris@gmail.com>
2023-10-03 10:38:41 +0200

    ssl feature extraction

---
+++ SSL_Features_Extraction/SSL_Features_Extraction_Audio_Modality/ssl_features_extraction_audio_modality/encoders/cnn1d.py
@@ -0,0 +1,129 @@
+import torch
+import torch.nn as nn
+from collections import OrderedDict
+
+from pytorch_lightning import LightningModule
+
+
+class CNN1D(LightningModule):
+    def __init__(self, 
+                in_channels, 
+                len_seq, 
+                out_channels=[32, 64, 128], 
+                kernel_sizes=[7, 5, 3], 
+                stride=1, 
+                padding=1,
+                pool_padding=0, 
+                pool_size=2, 
+                supervised=True,
+                relu_type = 'relu',
+                p_drop=0.2,
+                pretrained = None,
+                **kwargs):
+        """
+        1D-Convolutional Network with three layers
+        """
+        super(CNN1D, self).__init__()
+        self.len_seq = len_seq
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_sizes = kernel_sizes
+
+        self.name = 'cnn1d'
+        self.num_layers = len(out_channels)
+            
+        self.conv_block1 = nn.Sequential(OrderedDict([
+            ('conv1',nn.Conv1d(in_channels=in_channels, out_channels=out_channels[0], kernel_size=self.kernel_sizes[0], stride=stride, padding=padding)),
+            ('relu1', nn.ReLU(inplace=True)),
+            ('pool1', nn.MaxPool1d(kernel_size=pool_size, stride=None))
+        ]))
+
+        self.conv_block2 = nn.Sequential(OrderedDict([
+            ('conv2',nn.Conv1d(in_channels=out_channels[0], out_channels=out_channels[1], kernel_size=self.kernel_sizes[1], stride=stride, padding=padding)),
+            ('relu2', nn.ReLU(inplace=True)),
+            ('pool1', nn.MaxPool1d(kernel_size=pool_size, stride=None))
+        ]))
+
+        self.conv_block3 = nn.Sequential(OrderedDict([
+            ('conv3',nn.Conv1d(in_channels=out_channels[1], out_channels=out_channels[2], kernel_size=self.kernel_sizes[2], stride=stride, padding=padding)),
+            ('relu3', nn.ReLU(inplace=True)),
+            ('pool1', nn.MaxPool1d(kernel_size=pool_size, stride=None))
+        ]))
+
+        self.dropout = nn.Dropout(p=p_drop)
+
+        self.out_size = self._compute_out_size(len_seq, padding, self.kernel_sizes, stride, 3, out_channels[-1], pool_size, pool_padding)
+
+        if pretrained is not None:
+            self.load_state_dict(torch.load(pretrained))
+            print(f'succesfully loaded weights from {pretrained}')
+        else:
+            print("NO pretrained weights loaded")
+
+    @staticmethod
+    def _compute_out_size(sample_length, padding, kernel_sizes, stride, num_layers, num_channels, pool_size, pool_padding):
+        conv_out_size = sample_length
+        for i in range(num_layers):
+            conv_out_size = int((conv_out_size + 2 * padding - (kernel_sizes[i] - 1) - 1) / stride + 1)
+            conv_out_size = int((conv_out_size + 2 * pool_padding - (pool_size - 1) - 1) / pool_size + 1)
+        return int(num_channels * conv_out_size)
+
+    def forward(self, x):
+        x = self.conv_block1(x)
+        x = self.dropout(x)
+
+        x = self.conv_block2(x)
+        x = self.dropout(x)
+
+        x = self.conv_block3(x)
+        x = self.dropout(x)
+        return x
+
+
+class CNN1D1L(LightningModule):
+    def __init__(self, 
+                in_channels, 
+                len_seq, 
+                out_channels=[32], 
+                kernel_sizes=[7], 
+                stride=1, 
+                padding=1,
+                pool_padding=0, 
+                pool_size=2, 
+                supervised=True,
+                relu_type = 'relu',
+                p_drop=0.2,
+                **kwargs):
+        """
+        1D-Convolutional encoder with one convolutional layer
+        """
+        super(CNN1D1L, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_sizes = kernel_sizes
+
+        self.name = 'cnn1d'
+        self.num_layers = len(out_channels)
+            
+        self.conv_block1 = nn.Sequential(OrderedDict([
+            ('conv1',nn.Conv1d(in_channels=in_channels, out_channels=out_channels[0], kernel_size=self.kernel_sizes[0], stride=stride, padding=padding)),
+            ('relu1', nn.LeakyReLU(inplace=True)),
+            ('pool1', nn.MaxPool1d(kernel_size=pool_size, stride=None))
+        ]))
+
+        self.dropout = nn.Dropout(p=p_drop)
+
+        self.out_size = self._compute_out_size(len_seq, padding, self.kernel_sizes, stride, self.num_layers, out_channels[-1], pool_size, pool_padding)
+
+    @staticmethod
+    def _compute_out_size(sample_length, padding, kernel_sizes, stride, num_layers, num_channels, pool_size, pool_padding):
+        conv_out_size = sample_length
+        for i in range(num_layers):
+            conv_out_size = int((conv_out_size + 2 * padding - (kernel_sizes[i] - 1) - 1) / stride + 1)
+            conv_out_size = int((conv_out_size + 2 * pool_padding - (pool_size - 1) - 1) / pool_size + 1)
+        return int(num_channels * conv_out_size)
+
+    def forward(self, x):
+        x = self.conv_block1(x)
+        x = self.dropout(x)
+        return x
--- SSL_Features_Extraction/SSL_Features_Extraction_Audio_Modality/ssl_features_extraction_audio_modality/generate_features.py
+++ SSL_Features_Extraction/SSL_Features_Extraction_Audio_Modality/ssl_features_extraction_audio_modality/generate_features.py
@@ -1,24 +1,54 @@
 # Python code here
+import os
+import torch
+import scipy
+import pathlib
+import numpy as np
+import pandas as pd
 
+from tqdm import tqdm
 from conf import CUSTOM_SETTINGS
+from pytorch_lightning import Trainer, seed_everything
+from conf import CUSTOM_SETTINGS,MAIN_FOLDER,OUTPUTS_FOLDER
+from encoders.cnn1d import CNN1D,CNN1D1L
 
 
 def generate_ssl_features():
     """
-    A basic print function to verify if docker is running.
+    Function to extract SSL features and save to disk
     :return: None
     """
-    model_config = CUSTOM_SETTINGS['pre_processing']['some_config_preprocessing']
-    print(f'Docker for features generation Audio has run (Enabler 3). Conf from configuration.json file: {model_config}')
 
-    #load data
+    print(CUSTOM_SETTINGS)
+    splith_paths = {'train':"outputs/train.csv",'val':"outputs/val.csv",'test':"outputs/test.csv"}
 
-    #load model
+    encoder = CNN1D(
+        len_seq=CUSTOM_SETTINGS["pre_processing_config"]['max_length'] * CUSTOM_SETTINGS["pre_processing_config"]['target_sr'],
+        pretrained=CUSTOM_SETTINGS['encoder_config']['pretrained'] if "pretrained" in CUSTOM_SETTINGS['encoder_config'].keys() else None,
+        **CUSTOM_SETTINGS["encoder_config"]['kwargs']
+    )
+    encoder.eval()
+    print(encoder)
 
-    #iterate over data
-        # get features
-        # save features to disk
-        # write to csv
+    save_folder = os.path.join(OUTPUTS_FOLDER, 'SSL_Features_Exteraction', 'ssl_features')
+    pathlib.Path(save_folder).mkdir(parents=True, exist_ok=True)
+
+    #TODO: iterate over keys or userspecified csv/files ?
+    generate_and_save(encoder,splith_paths['train'],save_folder)
+    generate_and_save(encoder,splith_paths['val'],save_folder)
+    generate_and_save(encoder,splith_paths['test'],save_folder)
+
+
+def generate_and_save(encoder,csv_path,out_path):
+
+    meta_data = pd.read_csv(os.path.join(MAIN_FOLDER,csv_path))
+    for data_path in tqdm(meta_data['files']):
+        #TODO : change .wav to .npy and use numpy everywhere for reading in files
+        #TODO : find replacement for .replace('\\','/')) to have a seperator that works on all OS
+        x = scipy.io.wavfile.read(os.path.join(MAIN_FOLDER,data_path).replace('\\','/'))[1]
+        x_tensor = torch.tensor(np.expand_dims(x,axis=0))
+        features = encoder(x_tensor)
+        np.save(os.path.join(out_path,data_path.replace('\\','/').split(os.path.sep)[-1][:-3]+'npy'),features.detach().numpy())
 
 
 if __name__ == '__main__':
