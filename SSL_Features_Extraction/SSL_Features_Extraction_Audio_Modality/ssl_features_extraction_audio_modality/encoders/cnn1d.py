9adab983f5b7198c1aaebecf3ef0e9f0fc554684
PedroDKE <pedroantonio.jeuris@gmail.com>
2023-10-31 12:29:19 +0100

    change to variable length channels/kernel input

--- SSL_Features_Extraction/SSL_Features_Extraction_Audio_Modality/ssl_features_extraction_audio_modality/encoders/cnn1d.py
+++ SSL_Features_Extraction/SSL_Features_Extraction_Audio_Modality/ssl_features_extraction_audio_modality/encoders/cnn1d.py
@@ -6,30 +6,43 @@ from pytorch_lightning import LightningModule
 
 
 class CNN1D(LightningModule):
-    def __init__(self,
-                 in_channels,
-                 len_seq,
-                 out_channels=[32, 64, 128],
-                 kernel_sizes=[7, 5, 3],
-                 stride=1,
-                 padding=1,
-                 pool_padding=0,
-                 pool_size=2,
-                 p_drop=0.2,
-                 pretrained=None,
-                 **kwargs):
+    def __init__(self, 
+                in_channels, 
+                len_seq, 
+                out_channels=[32, 64, 128], 
+                kernel_sizes=[7, 5, 3], 
+                stride=1, 
+                padding=1,
+                pool_padding=0, 
+                pool_size=2, 
+                p_drop=0.2,
+                pretrained = None,
+                **kwargs):
         """
-        1D-Convolutional Network with three layers
-        Args:
-            in_channels: number of channels in the input data
-            len_seq: lenght of the input sequence
-            out_channels: list containing the number of channels in the convolutional layers
-            kernel_sizes: list containing the sizes of the convolutional kernels
-            strid: size of the stride
-            padding: unused, just to compute the out size
-            pool_size: size of the maxpooling 
-            p_drop: dropout value
-            pretrained: path to pretrained model
+        1D-Convolutional Network with three layers.
+
+        Parameters
+        ----------
+        in_channels : int
+            Number of channels in the input data.
+        len_seq : int
+            Length of the input sequence.
+        out_channels : list of int
+            List containing the number of channels in the convolutional layers.
+        kernel_sizes : list of int
+            List containing the sizes of the convolutional kernels.
+        stride : int
+            Size of the stride.
+        padding : int
+            Unused, just to compute the out size.
+        pool_padding : int
+            Padding for maxpooling.
+        pool_size : int
+            Size of the maxpooling.
+        p_drop : float
+            Dropout value.
+        pretrained : str
+            Path to pretrained model.
         """
         super(CNN1D, self).__init__()
         self.len_seq = len_seq
@@ -39,47 +52,34 @@ class CNN1D(LightningModule):
 
         self.name = 'cnn1d'
         self.num_layers = len(out_channels)
+        self.convolutional_blocks = OrderedDict()
 
-        self.conv_block1 = nn.Sequential(OrderedDict([
-            ('conv1', nn.Conv1d(in_channels=in_channels, out_channels=out_channels[0], kernel_size=self.kernel_sizes[0],
-                                stride=stride, padding=padding)),
-            ('relu1', nn.ReLU(inplace=True)),
-            ('pool1', nn.MaxPool1d(kernel_size=pool_size, stride=None))
-        ]))
+        assert len(out_channels)==len(kernel_sizes), "out_channel and kernel_size list lengths should match"
+        self.out_channels.insert(0,self.in_channels)
 
-        self.conv_block2 = nn.Sequential(OrderedDict([
-            ('conv2',
-             nn.Conv1d(in_channels=out_channels[0], out_channels=out_channels[1], kernel_size=self.kernel_sizes[1],
-                       stride=stride, padding=padding)),
-            ('relu2', nn.ReLU(inplace=True)),
-            ('pool1', nn.MaxPool1d(kernel_size=pool_size, stride=None))
-        ]))
+        for i in range(1,len(out_channels)):
+            self.convolutional_blocks.update({f'conv_block{i}':nn.Sequential(OrderedDict([
+                (f'conv{i}',nn.Conv1d(in_channels=out_channels[i-1], out_channels=out_channels[i], kernel_size=self.kernel_sizes[i-1], stride=stride, padding=padding)),
+                (f'relu{i}', nn.ReLU(inplace=True)),
+                (f'pool{i}', nn.MaxPool1d(kernel_size=pool_size, stride=None)),
+                (f'dropout{i}',nn.Dropout(p=p_drop))
+            ]))})
+        
+        self.convolutional_blocks = nn.Sequential(self.convolutional_blocks)
 
-        self.conv_block3 = nn.Sequential(OrderedDict([
-            ('conv3',
-             nn.Conv1d(in_channels=out_channels[1], out_channels=out_channels[2], kernel_size=self.kernel_sizes[2],
-                       stride=stride, padding=padding)),
-            ('relu3', nn.ReLU(inplace=True)),
-            ('pool1', nn.MaxPool1d(kernel_size=pool_size, stride=None))
-        ]))
-
-        self.dropout = nn.Dropout(p=p_drop)
-
-        self.out_size = self._compute_out_size(len_seq, padding, self.kernel_sizes, stride, 3, out_channels[-1],
-                                               pool_size, pool_padding)
+        self.out_size = self._compute_out_size(len_seq, padding, self.kernel_sizes, stride,len(self.out_channels)-1, out_channels[-1], pool_size, pool_padding)
 
         try:
             if pretrained is not None:
-                self.load_state_dict(torch.load(pretrained.replace('\\', '/')))
-                print(f'Successfully loaded weights from {pretrained}')
+                self.load_state_dict(torch.load(pretrained.replace('\\','/')))
+                print(f'succesfully loaded weights from {pretrained}')
             else:
-                print("No pretrained weights loaded")
+                print("NO pretrained weights loaded")
         except:
-            print(f'Failed to loaded weights from {pretrained}, encoder initialised with random weights')
+            print(f'failed to loaded weights from {pretrained}, encoder initialised with random weights')
 
     @staticmethod
-    def _compute_out_size(sample_length, padding, kernel_sizes, stride, num_layers, num_channels, pool_size,
-                          pool_padding):
+    def _compute_out_size(sample_length, padding, kernel_sizes, stride, num_layers, num_channels, pool_size, pool_padding):
         conv_out_size = sample_length
         for i in range(num_layers):
             conv_out_size = int((conv_out_size + 2 * padding - (kernel_sizes[i] - 1) - 1) / stride + 1)
@@ -87,73 +87,20 @@ class CNN1D(LightningModule):
         return int(num_channels * conv_out_size)
 
     def forward(self, x):
-        x = self.conv_block1(x)
-        x = self.dropout(x)
-
-        x = self.conv_block2(x)
-        x = self.dropout(x)
-
-        x = self.conv_block3(x)
-        x = self.dropout(x)
-        return x
-
-
-class CNN1D1L(LightningModule):
-    def __init__(self,
-                 in_channels,
-                 len_seq,
-                 out_channels=[32],
-                 kernel_sizes=[7],
-                 stride=1,
-                 padding=1,
-                 pool_padding=0,
-                 pool_size=2,
-                 p_drop=0.2,
-                 **kwargs):
         """
-        1D-Convolutional encoder with one convolutional layer
-        Args:
-            in_channels: number of channels in the input data
-            len_seq: lenght of the input sequence
-            out_channels: the number of channels in the convolutional layer
-            kernel_sizes: the size of the convolutional kernel
-            strid: size of the stride
-            padding: unused, just to compute the out size
-            pool_size: size of the maxpooling 
-            p_drop: dropout value
-            pretrained: path to pretrained model
+        Forward pass of the CNN1D.
 
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input tensor.
+
+        Returns
+        -------
+        torch.Tensor
+            Output tensor after applying CNN layers.
         """
-        super(CNN1D1L, self).__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.kernel_sizes = kernel_sizes
+        for conv_block in self.convolutional_blocks:
+            x = conv_block(x)
 
-        self.name = 'cnn1d'
-        self.num_layers = len(out_channels)
-
-        self.conv_block1 = nn.Sequential(OrderedDict([
-            ('conv1', nn.Conv1d(in_channels=in_channels, out_channels=out_channels[0], kernel_size=self.kernel_sizes[0],
-                                stride=stride, padding=padding)),
-            ('relu1', nn.LeakyReLU(inplace=True)),
-            ('pool1', nn.MaxPool1d(kernel_size=pool_size, stride=None))
-        ]))
-
-        self.dropout = nn.Dropout(p=p_drop)
-
-        self.out_size = self._compute_out_size(len_seq, padding, self.kernel_sizes, stride, self.num_layers,
-                                               out_channels[-1], pool_size, pool_padding)
-
-    @staticmethod
-    def _compute_out_size(sample_length, padding, kernel_sizes, stride, num_layers, num_channels, pool_size,
-                          pool_padding):
-        conv_out_size = sample_length
-        for i in range(num_layers):
-            conv_out_size = int((conv_out_size + 2 * padding - (kernel_sizes[i] - 1) - 1) / stride + 1)
-            conv_out_size = int((conv_out_size + 2 * pool_padding - (pool_size - 1) - 1) / pool_size + 1)
-        return int(num_channels * conv_out_size)
-
-    def forward(self, x):
-        x = self.conv_block1(x)
-        x = self.dropout(x)
         return x
--- SSL_Training/SSL_Audio_Modality/ssl_audio_modality/encoders/cnn1d.py
+++ SSL_Training/SSL_Audio_Modality/ssl_audio_modality/encoders/cnn1d.py
@@ -52,28 +52,22 @@ class CNN1D(LightningModule):
 
         self.name = 'cnn1d'
         self.num_layers = len(out_channels)
-            
-        self.conv_block1 = nn.Sequential(OrderedDict([
-            ('conv1',nn.Conv1d(in_channels=in_channels, out_channels=out_channels[0], kernel_size=self.kernel_sizes[0], stride=stride, padding=padding)),
-            ('relu1', nn.ReLU(inplace=True)),
-            ('pool1', nn.MaxPool1d(kernel_size=pool_size, stride=None))
-        ]))
+        self.convolutional_blocks = OrderedDict()
 
-        self.conv_block2 = nn.Sequential(OrderedDict([
-            ('conv2',nn.Conv1d(in_channels=out_channels[0], out_channels=out_channels[1], kernel_size=self.kernel_sizes[1], stride=stride, padding=padding)),
-            ('relu2', nn.ReLU(inplace=True)),
-            ('pool1', nn.MaxPool1d(kernel_size=pool_size, stride=None))
-        ]))
+        assert len(out_channels)==len(kernel_sizes), "out_channel and kernel_size list lengths should match"
+        self.out_channels.insert(0,self.in_channels)
 
-        self.conv_block3 = nn.Sequential(OrderedDict([
-            ('conv3',nn.Conv1d(in_channels=out_channels[1], out_channels=out_channels[2], kernel_size=self.kernel_sizes[2], stride=stride, padding=padding)),
-            ('relu3', nn.ReLU(inplace=True)),
-            ('pool1', nn.MaxPool1d(kernel_size=pool_size, stride=None))
-        ]))
+        for i in range(1,len(out_channels)):
+            self.convolutional_blocks.update({f'conv_block{i}':nn.Sequential(OrderedDict([
+                (f'conv{i}',nn.Conv1d(in_channels=out_channels[i-1], out_channels=out_channels[i], kernel_size=self.kernel_sizes[i-1], stride=stride, padding=padding)),
+                (f'relu{i}', nn.ReLU(inplace=True)),
+                (f'pool{i}', nn.MaxPool1d(kernel_size=pool_size, stride=None)),
+                (f'dropout{i}',nn.Dropout(p=p_drop))
+            ]))})
+        
+        self.convolutional_blocks = nn.Sequential(self.convolutional_blocks)
 
-        self.dropout = nn.Dropout(p=p_drop)
-
-        self.out_size = self._compute_out_size(len_seq, padding, self.kernel_sizes, stride, 3, out_channels[-1], pool_size, pool_padding)
+        self.out_size = self._compute_out_size(len_seq, padding, self.kernel_sizes, stride,len(self.out_channels)-1, out_channels[-1], pool_size, pool_padding)
 
         try:
             if pretrained is not None:
@@ -106,69 +100,7 @@ class CNN1D(LightningModule):
         torch.Tensor
             Output tensor after applying CNN layers.
         """
-        x = self.conv_block1(x)
-        x = self.dropout(x)
+        for conv_block in self.convolutional_blocks:
+            x = conv_block(x)
 
-        x = self.conv_block2(x)
-        x = self.dropout(x)
-
-        x = self.conv_block3(x)
-        x = self.dropout(x)
-        return x
-
-
-class CNN1D1L(LightningModule):
-    def __init__(self, 
-                in_channels, 
-                len_seq, 
-                out_channels=[32], 
-                kernel_sizes=[7], 
-                stride=1, 
-                padding=1,
-                pool_padding=0, 
-                pool_size=2, 
-                p_drop=0.2,
-                **kwargs):
-        """
-        1D-Convolutional Network with three layers
-        Args:
-            in_channels: number of channels in the input data
-            len_seq: lenght of the input sequence
-            out_channels: list containing the number of channels in the convolutional layers
-            kernel_sizes: list containing the sizes of the convolutional kernels
-            strid: size of the stride
-            padding: unused, just to compute the out size
-            pool_size: size of the maxpooling 
-            p_drop: dropout value
-            pretrained: path to pretrained model
-        """
-        super(CNN1D1L, self).__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.kernel_sizes = kernel_sizes
-
-        self.name = 'cnn1d'
-        self.num_layers = len(out_channels)
-            
-        self.conv_block1 = nn.Sequential(OrderedDict([
-            ('conv1',nn.Conv1d(in_channels=in_channels, out_channels=out_channels[0], kernel_size=self.kernel_sizes[0], stride=stride, padding=padding)),
-            ('relu1', nn.LeakyReLU(inplace=True)),
-            ('pool1', nn.MaxPool1d(kernel_size=pool_size, stride=None))
-        ]))
-
-        self.dropout = nn.Dropout(p=p_drop)
-
-        self.out_size = self._compute_out_size(len_seq, padding, self.kernel_sizes, stride, self.num_layers, out_channels[-1], pool_size, pool_padding)
-
-    @staticmethod
-    def _compute_out_size(sample_length, padding, kernel_sizes, stride, num_layers, num_channels, pool_size, pool_padding):
-        conv_out_size = sample_length
-        for i in range(num_layers):
-            conv_out_size = int((conv_out_size + 2 * padding - (kernel_sizes[i] - 1) - 1) / stride + 1)
-            conv_out_size = int((conv_out_size + 2 * pool_padding - (pool_size - 1) - 1) / pool_size + 1)
-        return int(num_channels * conv_out_size)
-
-    def forward(self, x):
-        x = self.conv_block1(x)
-        x = self.dropout(x)
         return x
