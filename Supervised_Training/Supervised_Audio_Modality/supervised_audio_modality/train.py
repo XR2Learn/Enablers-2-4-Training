1b59d70d10689fd7879a9165359eea9e05eb9408
bulatkh <bulatkhaert@gmail.com>
2023-11-03 17:30:25 +0100

    Clean up train script and utils initialization

--- Supervised_Training/Supervised_Audio_Modality/supervised_audio_modality/train.py
+++ Supervised_Training/Supervised_Audio_Modality/supervised_audio_modality/train.py
@@ -6,7 +6,7 @@ from pytorch_lightning import Trainer
 from conf import CUSTOM_SETTINGS, OUTPUTS_FOLDER, COMPONENT_OUTPUT_FOLDER, EXPERIMENT_ID
 from supervised_dataset import SupervisedDataModule
 from callbacks.setup_callbacks import setup_callbacks
-from utils.init_utils import (init_augmentations,init_transforms, init_encoder)
+from utils.init_utils import (init_augmentations, init_transforms, init_encoder)
 
 from classification_model import SupervisedModel
 from classifiers.linear import LinearClassifier
@@ -24,7 +24,7 @@ def run_supervised_training():
 
     # for now, don't use augmentations during supervised training
     augmentations = None
-    if (CUSTOM_SETTINGS['sup_config']['use_augmentations_in_sup'] == True) and (
+    if (CUSTOM_SETTINGS['sup_config']['use_augmentations_in_sup']) and (
             'augmentations' in CUSTOM_SETTINGS.keys()):
         augmentations = init_augmentations(CUSTOM_SETTINGS['augmentations'])
         print("Augmentations loaded successfully")
@@ -33,7 +33,7 @@ def run_supervised_training():
 
     datamodule = SupervisedDataModule(
         path=OUTPUTS_FOLDER,
-        input_type=CUSTOM_SETTINGS['encoder_config']['input_type'],
+        input_type=CUSTOM_SETTINGS['sup_config']['input_type'],
         batch_size=CUSTOM_SETTINGS['sup_config']['batch_size'],
         split=splith_paths,
         train_transforms=train_transforms,
@@ -43,51 +43,42 @@ def run_supervised_training():
         augmentations=augmentations
     )
     # initialise encoder
-    encoder = init_encoder(CUSTOM_SETTINGS["encoder_config"],
-                           CUSTOM_SETTINGS['encoder_config']['pretrained'] if "pretrained_path" in CUSTOM_SETTINGS[
-                               'encoder_config'].keys() else f"{OUTPUTS_FOLDER}/ssl_training/{EXPERIMENT_ID}_encoder.pt" if "pretrained_same_experiment" in
-                                                                                                                            CUSTOM_SETTINGS[
-                                                                                                                                'encoder_config'].keys() and
-                                                                                                                            CUSTOM_SETTINGS[
-                                                                                                                                'encoder_config'][
-                                                                                                                                "pretrained_same_experiment"] else None
-                           )
-    # CNN1D(
-    #    pretrained=CUSTOM_SETTINGS['encoder_config']['pretrained'] if "pretrained" in CUSTOM_SETTINGS['encoder_config'].keys() else None,
-    #    **CUSTOM_SETTINGS["encoder_config"]['kwargs']
-    # )
+    encoder = init_encoder(
+        model_cfg=CUSTOM_SETTINGS["encoder_config"],
+        ckpt_path=CUSTOM_SETTINGS['encoder_config']['pretrained'] if (
+            "pretrained_path" in CUSTOM_SETTINGS['encoder_config'].keys()
+        ) else f"{OUTPUTS_FOLDER}/ssl_training/{EXPERIMENT_ID}_encoder.pt" if (
+            "pretrained_same_experiment" in CUSTOM_SETTINGS['encoder_config'].keys()
+            and CUSTOM_SETTINGS['encoder_config']["pretrained_same_experiment"]
+        ) else None
+    )
 
     # add classification head to encoder
     classifier = LinearClassifier(encoder.out_size, CUSTOM_SETTINGS['dataset_config']['number_of_labels'])
     model = SupervisedModel(encoder=encoder, classifier=classifier, **CUSTOM_SETTINGS['sup_config']['kwargs'])
 
-    print(model)
-    # init callbacks  # initialize callbacks
+    # initialize callbacks
     callbacks = setup_callbacks(
         early_stopping_metric="val_loss",
         no_ckpt=False,
         patience=50,
+        num_classes=CUSTOM_SETTINGS['dataset_config']['number_of_labels']
     )
 
-    # initialize Pytorch-Lightning Training
+    # initialize Pytorch-Lightning Trainer
     trainer = Trainer(
-        # logger=loggers,
         accelerator='gpu' if torch.cuda.is_available() else 'cpu',
-        # devices=None if int(GPUS) == 0 else int(GPUS),
-        # devices=devices,
         deterministic=True,
         default_root_dir=os.path.join(COMPONENT_OUTPUT_FOLDER),
         callbacks=callbacks,
         max_epochs=CUSTOM_SETTINGS['sup_config']['epochs']
     )
 
-    # pre-train and report test loss
+    # train model and report metrics
     trainer.fit(model, datamodule)
-    # print(model.encoder.conv_block1.conv1.weight)
+
     # load in best weights
-    # model.load_from_checkpoint(callbacks[1].best_model_path,encoder=encoder,classifier=classifier)
     metrics = trainer.test(model, datamodule)
-    # print(model.encoder.conv_block1.conv1.weight)
     print(metrics)
 
     # save weights
