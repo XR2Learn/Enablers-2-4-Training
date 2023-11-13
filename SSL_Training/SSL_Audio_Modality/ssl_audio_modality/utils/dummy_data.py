b677259f236532df506129133d68ea7bb377731b
bulatkh <bulatkhaert@gmail.com>
2023-11-13 14:23:58 +0100

    Refactor utils and add tests

--- SSL_Training/SSL_Audio_Modality/ssl_audio_modality/utils/dummy_data.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def create_dummy_data(n_channels=1,length=2500,size=1000,n_labels=3):
-    dummy_data = np.zeros((size,n_channels,length))
-    dummy_labels = np.random.random_integers(low=0,high=n_labels,size=size)
-
-    return dummy_data,dummy_labels
