5738ca2c7ddd29172c0b5b64fe6f172038eb2112
bulatkh <bulatkhaert@gmail.com>
2023-11-15 14:18:05 +0100

    Refactoring and tests for SSL audio feature generation

---
+++ SSL_Features_Extraction/SSL_Features_Extraction_Audio_Modality/ssl_features_extraction_audio_modality/encoders/w2v.py
@@ -0,0 +1,124 @@
+from typing import List
+
+import torch
+import torchaudio
+import torch.nn as nn
+from pytorch_lightning import LightningModule
+
+from .cnn1d import CNN1D
+
+
+class Wav2Vec2Wrapper(LightningModule):
+    """ Wraps Wav2Vec 2.0 model from torch audio into the Lightning Module
+    """
+    def __init__(
+            self,
+            w2v2_type: str = 'base',
+            freeze: bool = True
+    ):
+        super().__init__()
+        # TODO: add other configurations of wav2vec2.0 and integrate with Wav2VecCNN
+        if w2v2_type == 'base':
+            bundle = torchaudio.pipelines.WAV2VEC2_BASE
+        else:
+            raise ValueError("wrong type of W2V2 model provided")
+        self.w2v2 = bundle.get_model()
+        if freeze:
+            for param in self.w2v2.parameters():
+                param.requires_grad = False
+
+    def forward(self, x, lengths=None):
+        internal_features, _ = self.w2v2.extract_features(x.squeeze(axis=1), lengths=lengths)
+        output_contextual_encoder, valid_lengths = self.w2v2.forward(x.squeeze(axis=1), lengths=lengths)
+        internal_features.append(output_contextual_encoder)
+        return internal_features, valid_lengths
+
+
+class Wav2Vec2CNN(LightningModule):
+    """ CNN applied on top of the wav2vec 2.0 features and weighted average
+        applied to different transformer layers from wav2vec.
+        Adapted from: https://arxiv.org/pdf/2104.03502.pdf
+    """
+    def __init__(
+            self,
+            length_samples: int = 10,
+            sample_rate: int = 16000,
+            w2v2_type: str = 'base',
+            freeze: bool = True,
+            out_channels: List = [128, 128],
+            kernel_sizes: List = [1, 1],
+            pretrained=None,
+    ):
+        super().__init__()
+
+        self.wav2vec2 = Wav2Vec2Wrapper(w2v2_type=w2v2_type, freeze=freeze)
+
+        self.weighted_average = nn.Parameter(torch.ones(13))  # for learnable weights
+
+        self.cnn = CNN1D(
+            in_channels=768,
+            len_seq=13,
+            out_channels=out_channels,
+            kernel_sizes=kernel_sizes,
+            padding=0,
+            stride=1,
+            pool_size=1,
+            pool_padding=0
+        )
+
+        self.out_size = self.compute_out_size(length=length_samples, sample_rate=sample_rate)
+
+        self.save_hyperparameters()
+
+        if pretrained is not None:
+            loaded_checkpoint = torch.load(pretrained.replace('\\', '/'))
+            # Pytorch lightning checkpoints store more values, and state dict needs to be accessed
+            # using "state_dict" key, whereas default pytorch checkpoints store state_dict only
+            if "state_dict" in loaded_checkpoint:
+                loaded_checkpoint = loaded_checkpoint["state_dict"]
+            self.load_state_dict(loaded_checkpoint)
+            print(f'succesfully loaded weights from {pretrained}')
+        else:
+            print("NO pretrained weights loaded")
+
+    def compute_out_size(self, length, sample_rate):
+        dummy_input = torch.rand((1, 1, length * sample_rate)).to(self.device)
+        out = self.forward(dummy_input)
+
+        return torch.numel(out)
+
+    def forward(self, x, lengths=None):
+        # pass data through wav2vec2
+        w2v2_features, valid_lengths = self.wav2vec2(x, lengths=lengths)
+        # process the features and apply weighted average
+        embedding = torch.stack(w2v2_features, axis=1)
+        embedding = embedding * self.weighted_average[None, :, None, None]
+        embedding = torch.sum(embedding, 1) / torch.sum(self.weighted_average)
+        # setting channels first
+        embedding = torch.transpose(embedding, 1, 2)
+
+        # zero-ing the invalid lengths
+        mask = torch.ones(embedding.shape).to(self.device)
+        if valid_lengths is not None:
+            for batch, valid in enumerate(valid_lengths):
+                mask[batch, :, valid:] = 0
+
+        masked_embedding = embedding * mask
+
+        # apply cnn layers
+        outs = self.cnn(masked_embedding)
+
+        mask = torch.ones(outs.shape).to(self.device)
+        if valid_lengths is not None:
+            for batch, valid in enumerate(valid_lengths):
+                mask[batch, :, valid:] = 0
+
+        # return global average
+        return masked_mean(outs, mask, dim=2)
+
+
+def masked_mean(tensor, mask, dim):
+    """from: https://discuss.pytorch.org/t/equivalent-of-numpy-ma-array-to-mask-values-in-pytorch/53354/5
+    """
+    masked = torch.mul(tensor, mask)  # Apply the mask using an element-wise multiply
+    return masked.sum(dim=dim) / mask.sum(dim=dim)
--- SSL_Features_Extraction/SSL_Features_Extraction_Audio_Modality/ssl_features_extraction_audio_modality/generate_features.py
+++ SSL_Features_Extraction/SSL_Features_Extraction_Audio_Modality/ssl_features_extraction_audio_modality/generate_features.py
@@ -1,13 +1,12 @@
 # Python code here
 import os
-import torch
 import pathlib
 import numpy as np
 import pandas as pd
 
 from tqdm import tqdm
 from conf import CUSTOM_SETTINGS, OUTPUTS_FOLDER, EXPERIMENT_ID
-from utils.init_utils import init_encoder
+from utils.init_utils import init_encoder, init_transforms
 
 
 def generate_ssl_features():
@@ -20,51 +19,69 @@ def generate_ssl_features():
     """
 
     print(CUSTOM_SETTINGS)
-    splith_paths = {'train': "train.csv", 'val': "val.csv", 'test': "test.csv"}
 
-    # TODO refactor the code below to not be hard coded, but use the component output folder as the path to load the
-    #  model
-    encoder = init_encoder(CUSTOM_SETTINGS["encoder_config"],
-                           CUSTOM_SETTINGS['encoder_config']['pretrained'] if "pretrained_path" in CUSTOM_SETTINGS[
-                               'encoder_config'].keys() else f"{OUTPUTS_FOLDER}/ssl_training/{EXPERIMENT_ID}_encoder.pt" if "pretrained_same_experiment" in
-                                                                                                                            CUSTOM_SETTINGS[
-                                                                                                                                'encoder_config'].keys() and
-                                                                                                                            CUSTOM_SETTINGS[
-                                                                                                                                'encoder_config'][
-                                                                                                                                "pretrained_same_experiment"] else None
-                           )
+    # currently, use custom train, val, test csv paths
+    data_paths = ["train.csv", "val.csv", "test.csv"]
+
+    encoder = init_encoder(
+        CUSTOM_SETTINGS["encoder_config"],
+        CUSTOM_SETTINGS['encoder_config']['pretrained'] if (
+            "pretrained_path" in CUSTOM_SETTINGS['encoder_config']
+        ) else f"{OUTPUTS_FOLDER}/ssl_training/{EXPERIMENT_ID}_encoder.pt" if (
+            "pretrained_same_experiment" in CUSTOM_SETTINGS['encoder_config'] and
+            CUSTOM_SETTINGS['encoder_config']["pretrained_same_experiment"]
+        ) else None
+    )
+
     encoder.eval()
     print(encoder)
 
     save_folder = os.path.join(OUTPUTS_FOLDER, 'SSL_features')
     pathlib.Path(save_folder).mkdir(parents=True, exist_ok=True)
 
-    # TODO: iterate over keys or userspecified csv/files ?
-    generate_and_save(encoder, splith_paths['train'], save_folder)
-    generate_and_save(encoder, splith_paths['val'], save_folder)
-    generate_and_save(encoder, splith_paths['test'], save_folder)
+    if 'transforms' in CUSTOM_SETTINGS.keys():
+        train_transforms, test_transforms = init_transforms(CUSTOM_SETTINGS['transforms'])
+        transforms = {
+            "train": train_transforms,
+            "val": train_transforms,
+            "test": test_transforms
+        }
+
+    for path_ in data_paths:
+        generate_and_save(encoder, path_, save_folder, transforms)
 
 
-def generate_and_save(encoder, csv_path, out_path):
+def generate_and_save(encoder, data_path, out_path, transforms):
     """
     generate_and_save : given the encoder, extract the features and save to .npy files
 
     Args:
         encoder: the pytorch encoder model to extract features from
-        csv_path: csv containing the paths to the files for which features have to be extracted and saved
+        data_path: can take two forms:
+            - csv containing the paths to the files for which features have to be extracted and saved
+            - path to a folder with .npy files
         out_path: output path to save the features to
     Returns:
         none
     """
+    if data_path.endswith(".csv") and not os.path.isdir(data_path):
+        files = pd.read_csv(os.path.join(OUTPUTS_FOLDER, data_path))['files']
+        filename = os.path.basename(data_path)
+        cur_transforms = transforms[filename.split(".")[0]]
+    elif os.path.isdir(data_path):
+        files = os.path.listdir(data_path)
+        cur_transforms = transforms["train"]
+    else:
+        raise ValueError("Incorrect data_path format")
 
-    meta_data = pd.read_csv(os.path.join(OUTPUTS_FOLDER, csv_path))
-    for data_path in tqdm(meta_data['files']):
-        # TODO : find replacement for .replace('\\','/')) to have a seperator that works on all OS
+    for data_path in tqdm(files):
         x = np.load(
-            os.path.join(OUTPUTS_FOLDER, CUSTOM_SETTINGS['ssl_config']['input_type'], data_path).replace('\\', '/'))
-        x_tensor = torch.tensor(np.expand_dims(x.T, axis=0) if len(x.shape) <= 1 else x.T)
+            os.path.join(OUTPUTS_FOLDER, CUSTOM_SETTINGS['ssl_config']['input_type'], data_path).replace('\\', '/')
+        )
+        if len(x.shape) <= 1:
+            x = np.expand_dims(x, axis=-1)
+        x_tensor = cur_transforms(x)
         features = encoder(x_tensor)
-        # print(data_path.split(os.path.sep))
         np.save(os.path.join(out_path, data_path.split(os.path.sep)[-1]), features.detach().numpy())
 
 
