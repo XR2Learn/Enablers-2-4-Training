714b377596eb819cfca780e92deed599201e59b1
Annanda Sousa <annanda.sousa@gmail.com>
2024-01-19 16:30:15 +0100

    Bump version: 0.3.0 → 0.3.1

--- Pre_processing/Pre_Processing_BM_Modality/setup.py
+++ Pre_processing/Pre_Processing_BM_Modality/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pre_processing_bm_modality',
-    version='0.3.0',
+    version='0.3.1',
     description='Pre-processing for BM Modality',
     author='UM-XR2Learn-Enablers',
     packages=['pre_processing_bm_modality'],
--- SSL_Training/SSL_Audio_Modality/setup.py
+++ SSL_Training/SSL_Audio_Modality/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ssl_audio_modality',
-    version='0.3.0',
+    version='0.3.1',
     description='Pre-training a SSL encoder model using audio data input type.',
     author='UM-XR2Learn-Enablers',
     packages=['ssl_audio_modality'],
