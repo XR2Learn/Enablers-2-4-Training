3e6b6c164f962a771fe35928b7fd41afa7c3e439
bulatkh <bulatkhaert@gmail.com>
2024-02-26 14:19:48 +0100

    Add SSL Feature Extraction component for bm

--- SSL_Features_Extraction/SSL_Features_Extraction_BM_Modality/ssl_features_extraction_bm_modality/generate_features.py
+++ SSL_Features_Extraction/SSL_Features_Extraction_BM_Modality/ssl_features_extraction_bm_modality/generate_features.py
@@ -1,12 +1,65 @@
-# Your Python code here
+# Python code here
+from conf import CUSTOM_SETTINGS, OUTPUTS_FOLDER, EXPERIMENT_ID
+from generate_and_save import generate_and_save
+from utils.init_utils import init_encoder, init_transforms
 
-from conf import MAIN_FOLDER
 
+def generate_ssl_features():
+    """
+    Function to extract SSL features and save to disk
+    Args:
+        None
+    Returns:
+        None
+    """
 
-def call_component():
-    print(MAIN_FOLDER)
-    print('Call component SSL Features Extraction BM Modality')
+    print(CUSTOM_SETTINGS)
+
+    # currently, use custom train, val, test csv paths
+    data_paths = ["train.csv", "val.csv", "test.csv"]
+
+    if "pretrained_path" in CUSTOM_SETTINGS['encoder_config']:
+        ckpt_path = CUSTOM_SETTINGS['encoder_config']['pretrained_path']
+    elif (
+        "pretrained_same_experiment" in CUSTOM_SETTINGS['encoder_config'] and
+        CUSTOM_SETTINGS['encoder_config']["pretrained_same_experiment"]
+    ):
+        ckpt_path = f"{OUTPUTS_FOLDER}/ssl_training/{EXPERIMENT_ID}_encoder.pt"
+    else:
+        raise ValueError("Pre-trained model checkpoint is not provided.")
+
+    try:
+        encoder = init_encoder(
+            CUSTOM_SETTINGS["encoder_config"],
+            ckpt_path
+        )
+    except:
+        raise ValueError("""
+                         The encoder cannot be initialized from the provided checkpoint.
+                         Make sure that checkpoint model architecture matches the model from configuration
+                         """)
+
+    encoder.eval()
+    print(encoder)
+
+    if 'transforms' in CUSTOM_SETTINGS.keys():
+        train_transforms, test_transforms = init_transforms(CUSTOM_SETTINGS['transforms'])
+        transforms = {
+            "train": train_transforms,
+            "val": train_transforms,
+            "test": test_transforms
+        }
+
+    for path_ in data_paths:
+        generate_and_save(
+            encoder,
+            path_,
+            OUTPUTS_FOLDER,
+            CUSTOM_SETTINGS["ssl_config"]["input_type"],
+            "SSL_features",
+            transforms
+        )
 
 
 if __name__ == '__main__':
-    call_component()
+    generate_ssl_features()
---
+++ SSL_Features_Extraction/SSL_Features_Extraction_BM_Modality/ssl_features_extraction_bm_modality/utils/init_utils.py
@@ -0,0 +1,52 @@
+import importlib
+from typing import Any, Dict, Optional
+
+from torchvision import transforms
+
+
+def init_encoder(model_cfg: Dict[str, Any], ckpt_path: Optional[str] = None):
+    """ Initialize (pre-trained) encoder from model configuration
+
+    Parameters
+    ----------
+    model_cfg : dict
+        configurations of the model: architecture and hyperparameters
+    ckpt_path : str, optional
+        path to the file with the pre-trained model, by default None
+
+    Returns
+    -------
+    torch.nn.Module
+        initialized encoder
+    """
+    module = importlib.import_module(f"{model_cfg['from_module']}")
+    class_ = getattr(module, model_cfg['class_name'])
+    return class_(**model_cfg['kwargs'], pretrained=ckpt_path)
+
+
+# Transforms and augmentations
+def init_transforms(transforms_cfg: Dict[str, Any]):
+    """ Initialize transforms from the provided configs
+    """
+    train = []
+    test = []
+    if transforms_cfg is not None:
+        for t in transforms_cfg:
+            module = importlib.import_module(f"utils.{t['from_module']}")
+            class_ = getattr(module, t['class_name'])
+
+            if "kwargs" in t:
+                transform = class_(**t['kwargs'])
+            else:
+                transform = class_()
+
+            train.append(transform)
+            if t['in_test']:
+                test.append(transform)
+
+            print(f"added {t['class_name']} transformation")
+
+    composed_train_transform = transforms.Compose(train)
+    composed_test_transform = transforms.Compose(test)
+
+    return composed_train_transform, composed_test_transform
