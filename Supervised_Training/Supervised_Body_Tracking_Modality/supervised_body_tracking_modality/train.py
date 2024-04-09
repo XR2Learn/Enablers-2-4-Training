7bf9a389083777732fa7f3f89a77a094567d63cc
Annanda Sousa <annanda.sousa@gmail.com>
2024-04-09 16:25:26 +0200

    Updating Supervised Training component for body tracking modality.

--- Supervised_Training/Supervised_Body_Tracking_Modality/supervised_body_tracking_modality/train.py
+++ Supervised_Training/Supervised_Body_Tracking_Modality/supervised_body_tracking_modality/train.py
@@ -1,6 +1,6 @@
 # Python code here
 
-from conf import CUSTOM_SETTINGS
+from conf import PATH_CUSTOM_SETTINGS
 
 
 def example_run():
@@ -8,9 +8,8 @@ def example_run():
     A basic print function to verify if docker is running.
     :return: None
     """
-    model_config = CUSTOM_SETTINGS['model']['some_config_model']
     print(
-        f'Docker for Ed-Training Body Tracking has run (Enabler 4). Conf from configuration.json file: {model_config}')
+        f'Docker for Ed-Training Body Tracking has run (Enabler 4). Conf from configuration.json file: {PATH_CUSTOM_SETTINGS}')
 
 
 if __name__ == '__main__':
