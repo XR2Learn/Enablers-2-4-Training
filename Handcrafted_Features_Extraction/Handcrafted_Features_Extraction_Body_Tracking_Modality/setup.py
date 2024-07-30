47354756ff92a0a8001ae75524acd5a0445d3379
Annanda Sousa <annanda.sousa@gmail.com>
2024-07-30 11:28:28 +0200

    Updating the author's name on setup.py files for the body tracking modality.

--- Handcrafted_Features_Extraction/Handcrafted_Features_Extraction_Body_Tracking_Modality/setup.py
+++ Handcrafted_Features_Extraction/Handcrafted_Features_Extraction_Body_Tracking_Modality/setup.py
@@ -4,7 +4,7 @@ setup(
     name='handcrafted_features_extraction_body_tracking_modality',
     version='0.5.0',
     description='Handcrafted Features Extraction component for the Body Tracking Modality from Magic XRoom data.',
-    author='UM-XR2Learn-Enablers',
+    author='SUPSI',
     packages=['handcrafted_features_extraction_body_tracking_modality'],
     zip_safe=False
 )
--- Supervised_Training/Supervised_Body_Tracking_Modality/setup.py
+++ Supervised_Training/Supervised_Body_Tracking_Modality/setup.py
@@ -4,7 +4,7 @@ setup(
     name='supervised_body_tracking_modality',
     version='0.5.0',
     description='Training an ED model using Body Tracking data input type.',
-    author='UM-XR2Learn-Enablers',
+    author='SUPSI',
     packages=['supervised_body_tracking_modality'],
     zip_safe=False
 )
