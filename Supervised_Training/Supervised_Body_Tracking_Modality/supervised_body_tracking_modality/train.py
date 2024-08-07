b4e7b9255bb9e8954503e94128b6fc54e9b0f6b8
bulatkh <bulatkhaert@gmail.com>
2024-08-07 10:13:36 +0200

    Fix CNN input shapes and add optimizer with lr

--- Supervised_Training/Supervised_Body_Tracking_Modality/supervised_body_tracking_modality/train.py
+++ Supervised_Training/Supervised_Body_Tracking_Modality/supervised_body_tracking_modality/train.py
@@ -8,6 +8,7 @@ from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.layers import Conv1D, MaxPooling1D, Flatten, Dense, Dropout
 from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
 
 from conf import CUSTOM_SETTINGS, MODALITY_FOLDER, COMPONENT_OUTPUT_FOLDER, EXPERIMENT_ID, modality
 
@@ -21,9 +22,9 @@ def run_supervised_training():
     val_data_path = os.path.join(MODALITY_FOLDER, 'val.csv')
 
     # Load data from CSV files using the paths
-    train_data = pd.read_csv(train_data_path)
-    test_data = pd.read_csv(test_data_path)
-    val_data = pd.read_csv(val_data_path)
+    train_data = pd.read_csv(train_data_path, index_col=0)
+    test_data = pd.read_csv(test_data_path, index_col=0)
+    val_data = pd.read_csv(val_data_path, index_col=0)
 
     # Separate features and target labels
     X_train = train_data.iloc[:, :-1].values
@@ -33,10 +34,10 @@ def run_supervised_training():
     X_val = val_data.iloc[:, :-1].values
     y_val = val_data.iloc[:, -1].values
 
-    # Reshape data for Conv1D: (num_samples, num_features, 1)
-    X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], 1))
-    X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], 1))
-    X_val = X_val.reshape((X_val.shape[0], X_val.shape[1], 1))
+    # Reshape data for Conv1D: (num_samples, segment_size, num_features)
+    X_train = X_train.reshape((X_train.shape[0], CUSTOM_SETTINGS["pre_processing_config"]["segment_size"], -1))
+    X_test = X_test.reshape((X_test.shape[0], CUSTOM_SETTINGS["pre_processing_config"]["segment_size"], -1))
+    X_val = X_val.reshape((X_val.shape[0], CUSTOM_SETTINGS["pre_processing_config"]["segment_size"], -1))
 
     # Encode string labels to integers
     label_encoder = LabelEncoder()
@@ -50,19 +51,31 @@ def run_supervised_training():
     # Build the 1D CNN Model
     activation_fcn = CUSTOM_SETTINGS["sup_config"]["activation"]
     model = Sequential([
-        Conv1D(filters=CUSTOM_SETTINGS["sup_config"]["filters"],
-               kernel_size=CUSTOM_SETTINGS["sup_config"]["kernel_size"], activation='relu',
-               input_shape=(X_train.shape[1], 1)),
+        Conv1D(
+            filters=CUSTOM_SETTINGS["sup_config"]["filters"],
+            kernel_size=CUSTOM_SETTINGS["sup_config"]["kernel_size"],
+            activation='relu',
+            input_shape=(X_train.shape[1], X_train.shape[2])
+        ),
         MaxPooling1D(pool_size=2),
         Flatten(),
         Dense(CUSTOM_SETTINGS["sup_config"]["dense_neurons"], activation=CUSTOM_SETTINGS["sup_config"]['activation']),
         Dropout(CUSTOM_SETTINGS["sup_config"]["dropout"]),
         Dense(num_classes, activation='softmax')
     ])
-    model.compile(optimizer=CUSTOM_SETTINGS["sup_config"]['optimizer_name'], loss='sparse_categorical_crossentropy',
-                  metrics=['accuracy'])
 
-    epoch = CUSTOM_SETTINGS["sup_config"]["epochs"]
+    if CUSTOM_SETTINGS["sup_config"]['optimizer_name'] == "adam":
+        opt = Adam(learning_rate=CUSTOM_SETTINGS["sup_config"].get("lr", 0.0003))
+    else:
+        raise ValueError("Optimizer is not supported.")
+
+    model.compile(
+        optimizer=opt,
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
+    epoch = CUSTOM_SETTINGS["sup_config"]["epochs"] * 5
     batch_size = CUSTOM_SETTINGS["sup_config"]["batch_size"]
 
     # Train the Model
